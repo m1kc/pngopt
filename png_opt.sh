c5baaa84325786713aaeda544d39ab19e245c0e1
m1kc (Max Musatov) <m1kc@yandex.ru>
2014-01-28 08:30:55 +0400

    Fix several issues.

--- png_opt.sh
+++ png_opt.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Recursive PNG optimizer
 # (c) m1kc, 2013
@@ -20,23 +20,23 @@
 
 d=0
 ARROW="==>"
-CR="pngcrush -q -rem allb -brute -ow -z 3 -zmem 9"
+CR="pngcrush -q -brute -ow"
 OPTI="optipng -quiet -o7"
 
 optimize(){
-	if [ -n "`ls ${1} | grep .png`" ]; then
-		echo "$ARROW" $CR ${1}/*.png
-		$CR ${1}/*.png
-		echo "$ARROW" $OPTI ${1}/*.png
-		$OPTI ${1}/*.png
+	if [ -n "$(ls "${1}" | grep .png)" ]; then
+		echo "$ARROW" "$CR" "${1}"/*.png
+		$CR "${1}"/*.png
+		echo "$ARROW" "$OPTI" "${1}"/*.png
+		$OPTI "${1}"/*.png
 	fi
 }
 
 process(){
 	let d=${d}+1
 	echo "[${d}] entering dir: ${1}"
-	optimize ${1}
-	for i in `ls ${1}`; do if [ -d ${1}/${i} ]; then process ${1}/${i}; fi; done
+	optimize "${1}"
+	for i in $(ls "${1}"); do if [ -d "${1}"/"${i}" ]; then process "${1}"/"${i}"; fi; done
 	echo "[${d}] leaving dir: ${1}"
 	let d=${d}-1
 }
